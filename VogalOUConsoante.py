a481274b30298c58fe95f54ede2e03603d0dfc2d
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2023-03-25 20:25:14 -0300

    Vogal ou Consoante
    Faça um Programa que verifique se uma letra digitada é vogal ou consoante.

---
+++ VogalOUConsoante.py
@@ -0,0 +1,16 @@
+#Masculino ou Feminino
+print('Digite se a pessoa é Homem ou Mulher.')
+x=input('Digite uma letra: ')
+a,e,i,o,u='a','e','i','o','u'
+if x==a:
+    print ('É vogal!')
+elif x==e:
+    print ('É vogal!')
+elif x==i:
+    print ('É vogal!')
+elif x==o:
+    print ('É vogal!')
+elif x==u:
+    print ('É vogal!')
+else:
+    print('É consoante!!')
