41776bfbba49719c57f8861e1632977a7eae9f07
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2023-06-15 16:51:26 -0300

    matriz _2

--- matrizes.py
+++ matrizes.py
@@ -1,5 +1,5 @@
 matriz = [[1,2,3],[4,5,6],[7,8,9]]
 for i in range (len(matriz)):
     for j in range (len(matriz[i])):
-        print(matriz[i][j])
-print(' ')
+        print(matriz[i][j], end=" ")
+    print()
